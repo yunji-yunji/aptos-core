9fbe539c6c9ec691f88b95cd3ee787953deb8761
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-11-14 17:23:16 -0400

    [fuzz] resolve build issue with custom toolchain

--- third_party/move/move-bytecode-verifier/src/regression_tests/pafl.sh
+++ third_party/move/move-bytecode-verifier/src/regression_tests/pafl.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-CURDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-TARGET=${CURDIR}/fuzz
 PREFIX=third_party/move
 
 while test $# -gt 0; do
     case "$1" in
         -b)
-            # clean up
             cargo +fuzz clean
-            rm -rf ${TARGET}
 
-            # build and run the test
-            PAFL=${TARGET}/build PAFL_TARGET_PREFIX=${PREFIX} \
-            cargo +fuzz test -j 1 \
+            PAFL=1 PAFL_TARGET_PREFIX=${PREFIX} \
+            cargo +fuzz test \
                 regression_tests::fuzz::miri_path_fuzz -- \
                 --exact
             ;;
         -r)
-            # run the fuzzer
-            PAFL=${TARGET}/build PAFL_TARGET_PREFIX=${PREFIX} \
+            PAFL=1 PAFL_TARGET_PREFIX=${PREFIX} \
             cargo +fuzz miri test \
                 regression_tests::fuzz::miri_path_fuzz -- \
                 --exact
