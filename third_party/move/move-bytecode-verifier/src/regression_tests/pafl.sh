7656245cca457376389760d381feda8fcf97d7e1
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-11-14 20:42:09 -0400

    [fuzz] udpate the custom script

--- third_party/move/move-bytecode-verifier/src/regression_tests/pafl.sh
+++ third_party/move/move-bytecode-verifier/src/regression_tests/pafl.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+CURDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+TARGET=${CURDIR}/fuzz
 PREFIX=third_party/move
 
 while test $# -gt 0; do
     case "$1" in
         -b)
+            # clean up
             cargo +fuzz clean
+            rm -rf ${TARGET}
 
-            PAFL=1 PAFL_TARGET_PREFIX=${PREFIX} \
-            cargo +fuzz test \
+            # build and run the test
+            PAFL=${TARGET}/build PAFL_TARGET_PREFIX=${PREFIX} \
+            cargo +fuzz test -j 1 \
                 regression_tests::fuzz::miri_path_fuzz -- \
                 --exact
             ;;
         -r)
-            PAFL=1 PAFL_TARGET_PREFIX=${PREFIX} \
+            # run the fuzzer
+            PAFL=${TARGET}/build PAFL_TARGET_PREFIX=${PREFIX} \
             cargo +fuzz miri test \
                 regression_tests::fuzz::miri_path_fuzz -- \
                 --exact
