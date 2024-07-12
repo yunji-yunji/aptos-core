bd10e42a200d5d5e3824eac7a9f421c2c437318a
Yunji Kim <yj1230k@gmail.com>
2024-07-11 21:48:46 -0400

    code refactoring

--- third_party/move/move-bytecode-verifier/src/regression_tests/fuzz.rs
+++ third_party/move/move-bytecode-verifier/src/regression_tests/fuzz.rs
@@ -1,13 +1,7 @@
-use std::env;
-use std::process;
-use std::error::Error;
-use std::io::{self, BufReader, Read, Seek, Write};
+use std::io::{self, Read,Write};
 use std::fs::{self, File};
 
-use serde_cbor::de::from_slice;
-use byteorder::{LittleEndian, ReadBytesExt};
-
-use move_binary_format::{file_format, CompiledModule};
+use move_binary_format::CompiledModule;
 use move_core_types::vm_status::StatusCode;
 use crate::verifier::verify_module;
 
@@ -44,14 +38,14 @@ fn miri_path_fuzz() {
     }
 }
 
-fn write_cm(module: &CompiledModule, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn write_cm_to_file(module: &CompiledModule, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let bytes = serde_cbor::to_vec(module)?;
     let mut file = File::create(file_path)?;
     file.write_all(&bytes)?;
     Ok(())
 }
 
-fn read_cm(file_path: &str) -> Result<CompiledModule, Box<dyn std::error::Error>> {
+fn read_cm_from_file(file_path: &str) -> Result<CompiledModule, Box<dyn std::error::Error>> {
     let mut file = File::open(file_path)?;
     let mut bytes = Vec::new();
     file.read_to_end(&mut bytes)?;
@@ -66,38 +60,3 @@ fn read_cm_stdin() -> Result<CompiledModule, Box<dyn std::error::Error>> {
     let module: CompiledModule = serde_cbor::from_slice(&bytes)?;
     Ok(module)
 }
-
-/*
-macro_rules! miri_path_fuzz_macro {
-    ($name:ident, $arg1:expr) => {
-        #[test]
-        fn $name() {
-            // let module = file_format::empty_module();
-            let module = match read_cm($arg1) {
-                Ok(compiledmodule) => { compiledmodule },
-                Err(_) => { panic!("cannot read module"); },
-            };
-            match verify_module(&module) {
-                Ok(_) => (),
-                Err(e) => {
-                    let status = e.major_status();
-                    // additionally force a panic on status code that should not been reached
-                    match status {
-                        StatusCode::UNKNOWN_VALIDATION_STATUS => unreachable!("UNKNOWN_VALIDATION_STATUS"),
-                        StatusCode::UNKNOWN_VERIFICATION_ERROR => unreachable!("UNKNOWN_VERIFICATION_ERROR"),
-                        StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR => unreachable!("UNKNOWN_INVARIANT_VIOLATION_ERROR"),
-                        StatusCode::UNREACHABLE => unreachable!("UNREACHABLE"),
-                        StatusCode::UNEXPECTED_ERROR_FROM_KNOWN_MOVE_FUNCTION => unreachable!("UNEXPECTED_ERROR_FROM_KNOWN_MOVE_FUNCTION"),
-                        StatusCode::VERIFIER_INVARIANT_VIOLATION => unreachable!("VERIFIER_INVARIANT_VIOLATION"),
-                        StatusCode::UNEXPECTED_VERIFIER_ERROR => unreachable!("UNEXPECTED_VERIFIER_ERROR"),
-                        StatusCode::UNEXPECTED_DESERIALIZATION_ERROR => unreachable!("UNEXPECTED_DESERIALIZATION_ERROR"),
-                        _ => (),
-                    }
-                }
-            }
-        }    
-    };
-}
-
-miri_path_fuzz_macro!(miri_path_fuzz1, &CM_PATH);
- */
